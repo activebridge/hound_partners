c01f1cc7d6b9002907347bc5ba44c38cb774fec8
Volodimir Partytskyi <volodimir.partytskyi@gmail.com>
2014-06-26 17:02:00 +0300

    Added random data for charts

--- app/assets/javascripts/column-drilldown.js
+++ app/assets/javascripts/column-drilldown.js
@@ -1,116 +1,115 @@
 $(function () {
+  Highcharts.data({
+      csv: document.getElementById('tsv').innerHTML,
+      itemDelimiter: '\t',
+      parsed: function (columns) {
 
-    Highcharts.data({
-        csv: document.getElementById('tsv').innerHTML,
-        itemDelimiter: '\t',
-        parsed: function (columns) {
+          var brands = {},
+              brandsData = [],
+              versions = {},
+              drilldownSeries = [];
 
-            var brands = {},
-                brandsData = [],
-                versions = {},
-                drilldownSeries = [];
+          // Parse percentage strings
+          columns[1] = $.map(columns[1], function (value) {
+              if (value.indexOf('%') === value.length - 1) {
+                  value = parseFloat(value);
+              }
+              return value;
+          });
 
-            // Parse percentage strings
-            columns[1] = $.map(columns[1], function (value) {
-                if (value.indexOf('%') === value.length - 1) {
-                    value = parseFloat(value);
-                }
-                return value;
-            });
+          $.each($.randomize(columns[0]), function (i, name) {
+              var brand,
+                  version;
 
-            $.each(columns[0], function (i, name) {
-                var brand,
-                    version;
+              if (i > 0) {
 
-                if (i > 0) {
+                  // Remove special edition notes
+                  name = name.split(' -')[0];
 
-                    // Remove special edition notes
-                    name = name.split(' -')[0];
+                  // Split into brand and version
+                  version = name.match(/([0-9]+[\.0-9x]*)/);
+                  if (version) {
+                      version = version[0];
+                  }
+                  brand = name.replace(version, '');
 
-                    // Split into brand and version
-                    version = name.match(/([0-9]+[\.0-9x]*)/);
-                    if (version) {
-                        version = version[0];
-                    }
-                    brand = name.replace(version, '');
+                  // Create the main data
+                  if (!brands[brand]) {
+                      brands[brand] = columns[1][i];
+                  } else {
+                      brands[brand] += columns[1][i];
+                  }
 
-                    // Create the main data
-                    if (!brands[brand]) {
-                        brands[brand] = columns[1][i];
-                    } else {
-                        brands[brand] += columns[1][i];
-                    }
+                  // Create the version data
+                  if (version !== null) {
+                      if (!versions[brand]) {
+                          versions[brand] = [];
+                      }
+                      versions[brand].push(['v' + version, columns[1][i]]);
+                  }
+              }
 
-                    // Create the version data
-                    if (version !== null) {
-                        if (!versions[brand]) {
-                            versions[brand] = [];
-                        }
-                        versions[brand].push(['v' + version, columns[1][i]]);
-                    }
-                }
+          });
 
-            });
+          $.each(brands, function (name, y) {
+              brandsData.push({
+                  name: name,
+                  y: y,
+                  drilldown: versions[name] ? name : null
+              });
+          });
+          $.each(versions, function (key, value) {
+              drilldownSeries.push({
+                  name: key,
+                  id: key,
+                  data: value
+              });
+          });
 
-            $.each(brands, function (name, y) {
-                brandsData.push({
-                    name: name,
-                    y: y,
-                    drilldown: versions[name] ? name : null
-                });
-            });
-            $.each(versions, function (key, value) {
-                drilldownSeries.push({
-                    name: key,
-                    id: key,
-                    data: value
-                });
-            });
+          // Create the chart
+          $('#column-drilldown').highcharts({
+              chart: {
+                  type: 'column'
+              },
+              title: {
+                  text: 'Browser market shares. November, 2013'
+              },
+              xAxis: {
+                  type: 'category'
+              },
+              yAxis: {
+                  title: {
+                      text: 'Total percent market share'
+                  }
+              },
+              legend: {
+                  enabled: false
+              },
+              plotOptions: {
+                  series: {
+                      borderWidth: 0,
+                      dataLabels: {
+                          enabled: true,
+                          format: '{point.y:.1f}%'
+                      }
+                  }
+              },
 
-            // Create the chart
-            $('#column-drilldown').highcharts({
-                chart: {
-                    type: 'column'
-                },
-                title: {
-                    text: 'Browser market shares. November, 2013'
-                },
-                xAxis: {
-                    type: 'category'
-                },
-                yAxis: {
-                    title: {
-                        text: 'Total percent market share'
-                    }
-                },
-                legend: {
-                    enabled: false
-                },
-                plotOptions: {
-                    series: {
-                        borderWidth: 0,
-                        dataLabels: {
-                            enabled: true,
-                            format: '{point.y:.1f}%'
-                        }
-                    }
-                },
+              tooltip: {
+                  headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+                  pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+              },
 
-                tooltip: {
-                    headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
-                    pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
-                },
+              series: [{
+                  name: 'Brands',
+                  colorByPoint: true,
+                  data: brandsData
+              }],
+              drilldown: {
+                  series: drilldownSeries
+              }
+          })
 
-                series: [{
-                    name: 'Brands',
-                    colorByPoint: true,
-                    data: brandsData
-                }],
-                drilldown: {
-                    series: drilldownSeries
-                }
-            })
-
-        }
-    });
+      }
+  });
 });
--- app/assets/javascripts/dashing/widgets/index.js
+++ app/assets/javascripts/dashing/widgets/index.js
@@ -5,3 +5,8 @@
 //= require column-drilldown
 //= require pie-donut
 //= require_tree .
+
+$.randomize = function(arr) {
+  for(var j, x, i = arr.length; i; j = parseInt(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+  return arr;
+};
